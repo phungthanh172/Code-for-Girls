8475ea0c9d6a423d2a5038730c2f5a3c8b23d571
phungthanh172 <phungthanh172@gmail.com>
2016-08-07 14:13:42 +0700

    Commit new version JDK 1.8 not no Kotin
    I create new project with Java JDK 1.8(not JDK for IntelliJ JDK) and
    don't choose and one option (no Kotin/ Grovy)

---
+++ sources/src/controllers/BoxController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import models.GameObject;
+import views.GameDrawer;
+
+/**
+ * Created by Ha San~ on 8/6/2016.
+ */
+public class BoxController extends SingleController implements Colliable {
+
+
+    public static final int SPEED = 1;
+
+    public BoxController(GameObject gameObject, GameDrawer gameDrawer) {
+        super(gameObject, gameDrawer);
+        this.gameVector.dx = -SPEED;
+        CollsionPool.instance.add(this);
+    }
+
+    @Override
+    public void onCollide(Colliable colliable) {
+
+    }
+}
